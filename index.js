03d58099e5df2a98bb6dc859bc5564b969a659da
marioagarcia <betomg87@gmail.com>
2015-03-04 01:06:34 -0700

    made the cities route

--- index.js
+++ index.js
@@ -29,6 +29,22 @@ app.get('/db', function(request, response) {
   });
 });
 
+app.get('/cities', function(request, response) {
+    var letters = request.query.q;
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+        client.query('SELECT city FROM cities WHERE city LIKE ' + letters, function(err, result) {
+            done();
+            if(err) {
+                console.error(err);
+                response.send("Error " + err);
+            }
+            else {
+                response.send(result.rows);
+            }
+        })
+    })
+});
+
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
 });
