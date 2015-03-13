4358e32969b0887b69de7e0288e1a87c46b44b71
marioagarcia <betomg87@gmail.com>
2015-03-12 23:34:57 -0600

    conformed to the specs on learning suite

--- public/js/weather.js
+++ public/js/weather.js
@@ -3,78 +3,78 @@
 // });
 
 $( "#city-input" ).keyup(function() {
-  var letters = $("#city-input").val();
-  var rest_url = "https://marios-weather-app.herokuapp.com/cities?q=" + letters;
-  $.getJSON(rest_url, function(data) {
+    var letters = $("#city-input").val();
+    var rest_url = "https://marios-weather-app.herokuapp.com/cities?q=" + letters;
+    $.getJSON(rest_url, function(data) {
 
-    var everything = "";
+        var everything = "";
 
-    $.each(data, function(i, item) {
-      everything += "<a href=\"\" class=\"list-group-item\">" + data[i].name + "</a>";
-    });
+        $.each(data, function(i, item) {
+            everything += "<a href=\"\" class=\"list-group-item\">" + data[i].name + "</a>";
+        });
 
-    if(everything != "" && letters != "") {
-      $("#suggestions").addClass("list-group-item");
-      $("#suggestions").html(everything);
-    }
-    else {
-      $("#suggestions").html("");
-      $("#suggestions").removeClass("list-group-item");
-    }
-    
-    console.log("did it!");
+        if(everything != "" && letters != "") {
+            $("#suggestions").addClass("list-group-item");
+            $("#suggestions").html(everything);
+        }
+        else {
+            $("#suggestions").html("");
+            $("#suggestions").removeClass("list-group-item");
+        }
 
-  })
-  .done(function() { console.log('getJSON request succeeded!'); })
-  .fail(function(jqXHR, textStatus, errorThrown) {
-    console.log('getJSON request failed! ' + textStatus);
-    console.log("incoming "+jqXHR.responseText);
-    $("#suggestions").html("");
-    $("#suggestions").removeClass("list-group-item");
-  })
-  .always(function() { console.log('getJSON request ended!');
-})
-.complete(function() { console.log("complete"); });
+        console.log("did it!");
+
+    })
+        .done(function() { console.log('getJSON request succeeded!'); })
+        .fail(function(jqXHR, textStatus, errorThrown) {
+            console.log('getJSON request failed! ' + textStatus);
+            console.log("incoming "+jqXHR.responseText);
+            $("#suggestions").html("");
+            $("#suggestions").removeClass("list-group-item");
+        })
+        .always(function() { console.log('getJSON request ended!');
+        })
+        .complete(function() { console.log("complete"); });
 });
 
 
 $("#city-btn").click(function(e) {
 
-  var city = $("#city-input").val();
+    var city = $("#city-input").val();
 
-  var wu_url = "https://api.wunderground.com/api/d91c5cfe30b71852/geolookup/conditions/q/UT/";
-  wu_url += city;
-  wu_url += ".json";
-  console.log(wu_url);
-  $.ajax({
-    url : wu_url,
-    dataType : "jsonp",
-    success : function(parsed_json) {
-      var location = parsed_json['location']['city'];
-      var temp_string = parsed_json['current_observation']['temperature_string'];
-      var current_weather = parsed_json['current_observation']['weather'];
-      var everything = "<ul>";
-      everything += "<li>Temperature: "+temp_string;
-      everything += "<li>Weather: "+current_weather;
-      everything += "</ul>";
-      $("#weather-pnl-title").html(location);
-      $("#weather-pnl-body").html(everything);
-    }
-  });
-  $("#chosen-city").val(city);
-});
-//$(document).ready(function() {
-    $('#post-btn').click(function () {
-        var obj = { name:$('#name-input').val(), comment:$('#comment').val() };
-        console.log('Posting: ' + obj);
-        //var url = 'https://marios-weather-app.herokuapp.com/comments';
-        var url = 'https://marios-weather-app.herokuapp.com/comments'
-        $.post(url, obj, function(data, status) {
-            console.log(status);
-        });
+    var wu_url = "https://api.wunderground.com/api/d91c5cfe30b71852/geolookup/conditions/q/UT/";
+    wu_url += city;
+    wu_url += ".json";
+    console.log(wu_url);
+    $.ajax({
+        url : wu_url,
+        dataType : "jsonp",
+        success : function(parsed_json) {
+            var location = parsed_json['location']['city'];
+            var temp_string = parsed_json['current_observation']['temperature_string'];
+            var current_weather = parsed_json['current_observation']['weather'];
+            var everything = "<ul>";
+            everything += "<li>Temperature: "+temp_string;
+            everything += "<li>Weather: "+current_weather;
+            everything += "</ul>";
+            $("#weather-pnl-title").html(location);
+            $("#weather-pnl-body").html(everything);
+        }
     });
-//});
-$('#comments').ready(function () {
+    $("#chosen-city").val(city);
+});
+
+$('#post-btn').click(function () {
+    var obj = { name:$('#name-input').val(), comment:$('#comment').val() };
+    $('#comment-posted').html(JSON.stringify(obj));
+    var url = 'https://marios-weather-app.herokuapp.com/comments';
+    $.post(url, obj, function(data, status) {
+        $('#post-status').html(status);
+    });
+});
+
+$('#comments-btn').click(function () {
+    $('#comments-btn').fadeOut();
     var url = 'https://marios-weather-app.herokuapp.com/comments';
     $.getJSON(url, function (comments) {
         var html = '';
